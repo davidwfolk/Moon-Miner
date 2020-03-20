db153b92166db7ef3c64650c168c5001d83a4556
David Folk <davidwfolk@gmail.com>
2020-03-20 16:37:34 -0600

    working on cheese modifier

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ let cheese = 0
 let digging = {
   "dig": 1
 }
-
+//NOTE fix values //
 let clickUpgrades = {
   pickaxes: {
     name: "pickaxes",
-    price: 100,
+    price: 1,
     quantity: 0,
-    multiplier: 1
+    multiplier: 10
   },
   shovels: {
     name: "shovels",
@@ -28,7 +28,7 @@ let automaticUpgrades = {
   },
   bulldozers: {
     name: "bulldozers",
-    price: 1500,
+    price: 5,
     quantity: 0,
     multiplier: 40
   }
@@ -41,34 +41,50 @@ let collectAutoUpgrades = {
 
 function mine(playerChoice) {
   let moonDig = digging[playerChoice]
-  let cheeseCount = cheese + moonDig
-    cheese = cheeseCount
-  console.log(cheeseCount);
-  if (cheeseCount < digging[playerChoice]){
-    cheeseCount = 0
-  }
-  document.getElementById("cheese-count").innerText = cheeseCount
+  cheese += moonDig
 
+   calcSingleUpgrade()
+   console.log(cheese);
+  // @ts-ignore
+  document.getElementById("cheese-count").innerText = cheese
 }
 
 //this is to purchase a single clickUpgrade -- need to increase the quantity of item purchased by 1//
 function purchaseSingleUpgrades(upgradeChoice){
+  let purchSing = clickUpgrades[upgradeChoice]
+  let buySing = purchSing + clickUpgrades[upgradeChoice].quantity
+    if (cheese >= clickUpgrades[upgradeChoice].price) {
+      clickUpgrades[upgradeChoice].quantity ++
+      cheese -= clickUpgrades[upgradeChoice].price
+      console.log(clickUpgrades[upgradeChoice].quantity);
+    }
 
+    drawClickUpgrades()
 
 }
 
 //this is to purhase the automaticUpgrades -- need to increase the quantity of item purchased
 function purchaseAutoUpgrades(autoUpgradeChoice) {
-
+  let purchSing = automaticUpgrades[autoUpgradeChoice]
+  let buySing = purchSing + automaticUpgrades[autoUpgradeChoice].quantity
+    if (cheese >= automaticUpgrades[autoUpgradeChoice].price) {
+      automaticUpgrades[autoUpgradeChoice].quantity ++
+      cheese -= automaticUpgrades[autoUpgradeChoice].price
+      console.log(automaticUpgrades[autoUpgradeChoice].quantity);
+}
+drawAutomaticUpgrades()
 }
 
 //this will calculate the modifier of the cheese from each clickUpgrade
-function calcSingleUpgrade (calcChoice) {
+function calcSingleUpgrade () {
 
+  for (const key in clickUpgrades) {
+    cheese += clickUpgrades[key].modifier * clickUpgrades[key].quantity
+} 
 }
 
-//this will calculate the automatic modifieer of the cheese from each AutomaticUpgrade
-function calcAutoUpgrade (autoCalcChoise) {
+//this will calculate the automatic modifier of the cheese from each AutomaticUpgrade
+function calcAutoUpgrade () {
 
 }
 
